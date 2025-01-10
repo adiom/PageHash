e9aa4054a392f616ea27c9e8852e50875c2843e2
Adiom Timur <adiom@canfly.org>
2025-01-10 13:49:45 +0400

    MITA

---
+++ pages/i/mita/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="mita-container">
+        <h1>Mita Page</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MitaPage',
+    data() {
+        return {
+            
+        }
+    },
+    methods: {
+        
+    }
+}
+</script>
+
+<style scoped>
+.mita-container {
+    padding: 20px;
+}
+</style>
